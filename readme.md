627fdc555081ba5b645da4aec73c6f711c5b006c
HexhamAllstar <phil_bingham45@hotmail.com>
2018-03-15 14:49:36 +0000

    Updated function inputs and defaults

--- readme.md
+++ readme.md
@@ -5,6 +5,8 @@ NBA matches this season, including the final result and the player specific box 
 To run this code do the following:
 1. Clone the repository and create a virtual environment with the packages specified in requirements.txt
 2. Download geckodriver.exe for your version of firefox into the same directory.
-3. Open scraper_run.py and modify the timedelta and hard-coded date in the while loop to choose the dates to get data between. The code will not download duplicate data.
-4. Run scraper_run.py. **Warning this can take several hours if you are scraping the whole season**.
-5. Sometimes the geckodriver will stop responding, in which case it won't find any games. If this happens then just change the timedelta and re-run the code from the last good date.
+3. Open an Ipython console and import scraper_run.py (or specifically the run_scraper function within). Run run_scraper(start_date, end_date) where start_date and end_date are strings of the format "dd/mm/yyyy" that specify the dates that you want data between. Start_date should be chronologically earlier than end_date. **Warning: If you scrape the entire season it will take well over an hour**
+4. The script will then grab the relevant data and store it in a local SQL database named NBA_data.db. This repository has the database up to date as of the last commit incase you do not want to wait for the scraper to run.
+5. backend.py contains some simple functions for retrieving data from the database. E.g. In a Jupyter notebook, use data = backend.retrieve_all_results() to get all of the team score data and data = backend.retrieve_all_boxscores() to get the player specific data.
+
+Note: Sometimes Selenium will hang. If this happens then exit the script and simply run again with the same dates. The script will not insert duplicate data.
--- scraper_run.py
+++ scraper_run.py
@@ -5,66 +5,72 @@ from datetime import datetime, timedelta
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
-options = Options()
-options.add_argument("--headless") # create headless option so browser doesn't open on screen.
-# create a firefox selenium driver
-driver = webdriver.Firefox(firefox_options=options, executable_path="geckodriver.exe")
-# get todays date
-date = datetime.now()
-# use timedelta if you want to start at a specific date
-date = date - timedelta(days=0)
 
-# initialise a counter so we can reopen driver every now and then
-# I have found that this improves speed and stability
-loop_counter = 0
+def run_scraper(start_date="17/10/2017", end_date=datetime.strftime(datetime.now(), "%d/%m/%Y")):
+    """This function takes in a start date and end date, in the format "dd/mm/yyyy", then uses functions in scraper_funcs
+    to scrape match result and boxscore data for all games between the 2 dates from the NBA website.
+    If a start date is not specified, the function assumes the start date will be the first day of the 2017/2018 season.
+    If an end date is not specified, the function assumes the end date will be todays date."""
 
-# loop while the date is later than 17th Oct 2017, first day of the season
-while date >= datetime.strptime("17/10/2017", "%d/%m/%Y"):
+    options = Options()
+    options.add_argument("--headless") # create headless option so browser doesn't open on screen.
+    # create a firefox selenium driver
+    driver = webdriver.Firefox(firefox_options=options, executable_path="geckodriver.exe")
+    # get todays date
+    date = datetime.strptime(end_date)
 
-    if (loop_counter % 20) == 0: # every 20 loops close and reopen driver
-        driver.close()
-        driver = webdriver.Firefox(firefox_options=options, executable_path="geckodriver.exe")
-    else:
-        pass
+    # initialise a counter so we can reopen driver every now and then
+    # I have found that this improves speed and stability
+    loop_counter = 0
 
-    # get all results from the database to check if the current date is already in there
-    results = backend.retrieve_all_results()
-    if not results.empty:
-        dates = results['GameDate']
-        dates = dates.apply(lambda d:datetime.strptime(d.split(' ')[0], '%Y-%m-%d').date())
-        dates = dates.values
-    else:
-        dates = []
+    # loop while the date is later than start date
+    while date >= datetime.strptime(start_date,"%d/%m/%Y"):
 
-    # compare current date with list of dates in the results df
-    # if it's already there then skip to avoid duplicates
-    if date.date() in dates:
-        datetest = (date.date() == dates)
-        print(date.strftime('%d/%m/%Y') + ' is already in the database.')
-        # create a count of how many games are stored in the database vs how many there should be on this date.
-        print('There are ' + str(sum(datetest)) + ' games on this date in the database.')
-        links = scraper_funcs.get_boxscore_links(date, driver)
-        print('There should be ' + str(len(links) -1) + ' games. Consider deleting this date and redownloading if these do not match.')
-    # if the current date isn't in the database, go grab the data and insert it
-    else:
-        # get a list of links for all boxscore buttons on the scores page for the current day
-        links = scraper_funcs.get_boxscore_links(date, driver)
-        # the first link is always blank, so length 1 means no real links
-        if len(links) == 1:
-            print(date.strftime('%d/%m/%Y') +  ' : No games played (or no games played yet).')
+        if (loop_counter > 0) & ((loop_counter % 20) == 0): # every 20 loops close and reopen driver
+            driver.close()
+            driver = webdriver.Firefox(firefox_options=options, executable_path="geckodriver.exe")
         else:
-            # remove blank from front of list of links
-            print(date.strftime('%d/%m/%Y') + ' : ' + str(len(links) - 1) +  ' game(s) played.')
-            print('Scraping ....')
-            links = links[1:]
-            #iterate through the boxscore links, adding resulting dataframes to our database
-            driver2 = webdriver.Firefox(firefox_options=options, executable_path="geckodriver.exe")
-            scraper_funcs.scrape_and_add(links, driver2) # do the scraping and add results to db
-            driver2.close() # we use a new driver for every date, reduces crashing
-            
-    # once we've added all of the boxscores for the current date, remove 1 from the datetime
-    date = date - timedelta(days=1)
+            pass
+
+        # get all results from the database to check if the current date is already in there
+        results = backend.retrieve_all_results()
+        if not results.empty:
+            dates = results['GameDate']
+            dates = dates.apply(lambda d:datetime.strptime(d.split(' ')[0], '%Y-%m-%d').date())
+            dates = dates.values
+        else:
+            dates = []
+
+        # compare current date with list of dates in the results df
+        # if it's already there then skip to avoid duplicates
+        if date.date() in dates:
+            datetest = (date.date() == dates)
+            print(date.strftime('%d/%m/%Y') + ' is already in the database.')
+            # create a count of how many games are stored in the database vs how many there should be on this date.
+            print('There are ' + str(sum(datetest)) + ' games on this date in the database.')
+            links = scraper_funcs.get_boxscore_links(date, driver)
+            print('There should be ' + str(len(links) -1) + ' games. Consider deleting this date and redownloading if these do not match.')
+        # if the current date isn't in the database, go grab the data and insert it
+        else:
+            # get a list of links for all boxscore buttons on the scores page for the current day
+            links = scraper_funcs.get_boxscore_links(date, driver)
+            # the first link is always blank, so length 1 means no real links
+            if len(links) == 1:
+                print(date.strftime('%d/%m/%Y') +  ' : No games played (or no games played yet).')
+            else:
+                # remove blank from front of list of links
+                print(date.strftime('%d/%m/%Y') + ' : ' + str(len(links) - 1) +  ' game(s) played.')
+                print('Scraping ....')
+                links = links[1:]
+                #iterate through the boxscore links, adding resulting dataframes to our database
+                driver2 = webdriver.Firefox(firefox_options=options, executable_path="geckodriver.exe")
+                scraper_funcs.scrape_and_add(links, driver2) # do the scraping and add results to db
+                driver2.close() # we use a new driver for every date, reduces crashing
+
+        # once we've added all of the boxscores for the current date, remove 1 from the datetime
+        date = date - timedelta(days=1)
 
 
-# after all of the loops, close the webdriver
-driver.close()
+    # after all of the loops, close the webdriver
+    driver.close()
+    print('Oldest date reached, scraping finished.')
